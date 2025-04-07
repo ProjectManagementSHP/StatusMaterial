328850e300863600c685c098f4bfed882b8588c6
Andres <andres.duarte@specializedharness.com>
2025-04-07 11:33:03 -0600

    Programa base

---
+++ Status_material/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txttag = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnasignar = New System.Windows.Forms.Button()
+        Me.btnentrada = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.lblPN = New System.Windows.Forms.Label()
+        Me.lblBalance = New System.Windows.Forms.Label()
+        Me.lblTAG = New System.Windows.Forms.Label()
+        Me.TxtNumEmpleado = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txttag
+        '
+        Me.txttag.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txttag.Location = New System.Drawing.Point(157, 18)
+        Me.txttag.Margin = New System.Windows.Forms.Padding(4)
+        Me.txttag.Name = "txttag"
+        Me.txttag.Size = New System.Drawing.Size(295, 34)
+        Me.txttag.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(67, 21)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 31)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "TAG:"
+        '
+        'btnasignar
+        '
+        Me.btnasignar.Enabled = False
+        Me.btnasignar.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnasignar.Location = New System.Drawing.Point(73, 369)
+        Me.btnasignar.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnasignar.Name = "btnasignar"
+        Me.btnasignar.Size = New System.Drawing.Size(133, 48)
+        Me.btnasignar.TabIndex = 2
+        Me.btnasignar.Text = "Asignar"
+        Me.btnasignar.UseVisualStyleBackColor = True
+        '
+        'btnentrada
+        '
+        Me.btnentrada.Enabled = False
+        Me.btnentrada.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnentrada.Location = New System.Drawing.Point(389, 369)
+        Me.btnentrada.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnentrada.Name = "btnentrada"
+        Me.btnentrada.Size = New System.Drawing.Size(140, 48)
+        Me.btnentrada.TabIndex = 3
+        Me.btnentrada.Text = "Entrada"
+        Me.btnentrada.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.Location = New System.Drawing.Point(76, 119)
+        Me.lblStatus.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(100, 31)
+        Me.lblStatus.TabIndex = 4
+        Me.lblStatus.Text = "Status:"
+        '
+        'lblPN
+        '
+        Me.lblPN.AutoSize = True
+        Me.lblPN.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPN.Location = New System.Drawing.Point(117, 161)
+        Me.lblPN.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblPN.Name = "lblPN"
+        Me.lblPN.Size = New System.Drawing.Size(60, 31)
+        Me.lblPN.TabIndex = 5
+        Me.lblPN.Text = "PN:"
+        '
+        'lblBalance
+        '
+        Me.lblBalance.AutoSize = True
+        Me.lblBalance.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBalance.Location = New System.Drawing.Point(53, 203)
+        Me.lblBalance.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblBalance.Name = "lblBalance"
+        Me.lblBalance.Size = New System.Drawing.Size(120, 31)
+        Me.lblBalance.TabIndex = 6
+        Me.lblBalance.Text = "Balance:"
+        '
+        'lblTAG
+        '
+        Me.lblTAG.AutoSize = True
+        Me.lblTAG.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTAG.Location = New System.Drawing.Point(175, 245)
+        Me.lblTAG.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblTAG.Name = "lblTAG"
+        Me.lblTAG.Size = New System.Drawing.Size(97, 31)
+        Me.lblTAG.TabIndex = 7
+        Me.lblTAG.Text = "lblTAG"
+        '
+        'TxtNumEmpleado
+        '
+        Me.TxtNumEmpleado.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtNumEmpleado.Location = New System.Drawing.Point(157, 66)
+        Me.TxtNumEmpleado.Margin = New System.Windows.Forms.Padding(4)
+        Me.TxtNumEmpleado.Name = "TxtNumEmpleado"
+        Me.TxtNumEmpleado.Size = New System.Drawing.Size(295, 34)
+        Me.TxtNumEmpleado.TabIndex = 10
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(28, 66)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 31)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Usuario:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(621, 459)
+        Me.Controls.Add(Me.TxtNumEmpleado)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblTAG)
+        Me.Controls.Add(Me.lblBalance)
+        Me.Controls.Add(Me.lblPN)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnentrada)
+        Me.Controls.Add(Me.btnasignar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txttag)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Cambia Estatus del Material"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txttag As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnasignar As Button
+    Friend WithEvents btnentrada As Button
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents lblPN As Label
+    Friend WithEvents lblBalance As Label
+    Friend WithEvents lblTAG As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtNumEmpleado As TextBox
+End Class
---
+++ Status_material/Form1.vb
@@ -0,0 +1,196 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private strCnn As String = "Server=10.17.182.12\SQLEXPRESS2012;Database=SEA;User ID=sa;Password=SHPadmin14%"
+    'Private strCnn As String = "Server=10.17.182.36\SQLEXPRESS2012;Database=SEA;User ID=sa;Password=SHPadmin14%"
+    'Private strCnn As String = "Server=SHPLAPSIS01\SQLEXPRESS2012; Database=SEA; User ID=sa;Password=SHPadmin14%"
+    Private cnn As New SqlConnection(strCnn)
+
+    Private Sub btnasignar_Click(sender As Object, e As EventArgs) Handles btnasignar.Click
+
+
+
+
+        System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.WaitCursor
+        UpdateTag(lblTAG.Text, "NoAvailable")
+        UpdateUsuarioSalida(lblTAG.Text)
+        System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.Default
+    End Sub
+
+
+    Private Sub TxtNumEmpleado_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtNumEmpleado.KeyPress
+
+        If Asc(e.KeyChar) = 13 Then
+            BuscarTag(txttag.Text)
+        End If
+
+    End Sub
+
+
+
+    Private Sub btnentrada_Click(sender As Object, e As EventArgs) Handles btnentrada.Click
+        System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.WaitCursor
+        UpdateTag(lblTAG.Text, "Available")
+        UpdateUsuarioEntrada(lblTAG.Text)
+        'MessageBox.Show("El Tag solo se puede regresar por la aplicacion", "", MessageBoxButtons.OK)
+        System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.Default
+    End Sub
+
+    Private Sub txttag_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txttag.KeyPress
+        System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.WaitCursor
+        If Asc(e.KeyChar) = 13 Then
+            BuscarTag(txttag.Text)
+            TxtNumEmpleado.Focus()
+        End If
+        System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.Default
+
+
+
+    End Sub
+
+    Private Sub txttag_TextChanged(sender As Object, e As EventArgs) Handles txttag.TextChanged
+        System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.WaitCursor
+
+        System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.Default
+    End Sub
+
+    Private Sub BuscarTag(ByVal TAG As String)
+        Using TN As New DataTable
+            Dim Edo As String = ""
+            Try 'Query para encontrar los BOM que no se han usado
+                Dim cmd As SqlCommand
+                Dim dr As SqlDataReader
+                Dim Query As String = "SELECT * FROM tblItemsTags WHERE TAG=@TAG "
+                cmd = New SqlCommand(Query, cnn)
+                cmd.CommandType = CommandType.Text
+                cmd.Parameters.Add("@TAG", SqlDbType.NVarChar).Value = TAG
+                'cmd.Parameters.Add("@Hasta", SqlDbType.NVarChar).Value = Hasta
+                cnn.Open()
+                dr = cmd.ExecuteReader
+                TN.Load(dr)
+                cnn.Close()
+                If TN.Rows.Count > 0 Then
+                    lblTAG.Text = txttag.Text
+                    lblStatus.Text = "Status: " + TN.Rows(0).Item("Status").ToString
+                    lblPN.Text = "PN: " + TN.Rows(0).Item("PN").ToString
+                    lblBalance.Text = "Balance: " + CStr(Math.Round(CDec(Val(TN.Rows(0).Item("Balance").ToString)), 1)) + "   " + TN.Rows(0).Item("Unit").ToString
+                    If TN.Rows(0).Item("Status").ToString = "Available" And TxtNumEmpleado.Text <> "" Then
+                        btnasignar.Enabled = True
+                        btnentrada.Enabled = False
+                    ElseIf TN.Rows(0).Item("Status").ToString = "NoAvailable" And TxtNumEmpleado.Text <> "" Then
+                        btnasignar.Enabled = False
+                        btnentrada.Enabled = True
+                    End If
+                End If
+            Catch ex As Exception
+                Edo = cnn.State.ToString
+                If Edo = "Open" Then cnn.Close()
+                MessageBox.Show(ex.Message + vbNewLine + "Error Loading SearchingTags", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub UpdateTag(ByVal TAG As String, ByVal Estatus As String)
+        Try
+            Dim query As String = "UPDATE tblItemsTags SET Status=@Status, InDate = GETDATE() WHERE TAG = @TAG "
+            If Estatus = "Available" Then
+                query = "UPDATE tblItemsTags SET Status=@Status, InDate=GETDATE(), OutDate=NULL WHERE TAG=@TAG "
+            ElseIf Estatus = "NoAvailable" Then
+                query = "UPDATE tblItemsTags SET Status=@Status, OutDate=GETDATE() WHERE TAG=@TAG "
+            End If
+            Dim cmd = New SqlCommand(query, cnn)
+            cmd.CommandType = CommandType.Text
+            cmd.Parameters.Add("@Status", SqlDbType.NVarChar).Value = Estatus
+            cmd.Parameters.Add("@TAG", SqlDbType.NVarChar).Value = TAG
+            cnn.Open()
+            cmd.ExecuteNonQuery()
+            cnn.Close()
+            BuscarTag(txttag.Text)
+            txttag.Text = ""
+            btnasignar.Enabled = False
+            btnentrada.Enabled = False
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+    Private Sub UpdateUsuarioSalida(ByVal TAG As String)
+        Try
+
+
+
+
+
+            Dim query As String = "UPDATE tblitemstags " &
+                      "SET SalidaUsuario = (CASE " &
+                      "                         WHEN (SELECT COUNT(*) FROM tblEmpleados WHERE EmployeeNumber = @EmployeeNumber) > 0 " &
+                      "                         THEN (SELECT FirstName + ' ' + MiddleName + ' ' + LastName + ' ' + MaternalLastName " &
+                      "                               FROM tblEmpleados WHERE EmployeeNumber = @EmployeeNumber) " &
+                      "                         ELSE @EmployeeNumber " &
+                      "                     END) " &
+                      "WHERE tag = @tag"
+
+
+
+            Dim cmd = New SqlCommand(query, cnn)
+            cmd.CommandType = CommandType.Text
+            cmd.Parameters.Add("@EmployeeNumber", SqlDbType.NVarChar).Value = TxtNumEmpleado.Text()
+            cmd.Parameters.Add("@TAG", SqlDbType.NVarChar).Value = TAG
+            cnn.Open()
+            cmd.ExecuteNonQuery()
+            cnn.Close()
+            BuscarTag(txttag.Text)
+            txttag.Text = ""
+            TxtNumEmpleado.Text = ""
+            btnasignar.Enabled = False
+            btnentrada.Enabled = False
+
+            txttag.Focus()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub UpdateUsuarioEntrada(ByVal TAG As String)
+        Try
+
+
+
+
+
+            Dim query As String = "UPDATE tblitemstags " &
+                      "SET EntradaUsuario = (CASE " &
+                      "                         WHEN (SELECT COUNT(*) FROM tblEmpleados WHERE EmployeeNumber = @EmployeeNumber) > 0 " &
+                      "                         THEN (SELECT FirstName + ' ' + MiddleName + ' ' + LastName + ' ' + MaternalLastName " &
+                      "                               FROM tblEmpleados WHERE EmployeeNumber = @EmployeeNumber) " &
+                      "                         ELSE @EmployeeNumber " &
+                      "                     END) " &
+                      "WHERE tag = @tag"
+
+
+
+            Dim cmd = New SqlCommand(query, cnn)
+            cmd.CommandType = CommandType.Text
+            cmd.Parameters.Add("@EmployeeNumber", SqlDbType.NVarChar).Value = TxtNumEmpleado.Text()
+            cmd.Parameters.Add("@TAG", SqlDbType.NVarChar).Value = TAG
+            cnn.Open()
+            cmd.ExecuteNonQuery()
+            cnn.Close()
+            BuscarTag(txttag.Text)
+            txttag.Text = ""
+            TxtNumEmpleado.Text = ""
+            btnasignar.Enabled = False
+            btnentrada.Enabled = False
+
+            txttag.Focus()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblTAG.Text = ""
+    End Sub
+End Class
