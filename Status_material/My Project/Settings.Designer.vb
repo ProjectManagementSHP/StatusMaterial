57369645eb0f76bcb64b3546a67e41a8d80461d2
Jonathan Berumen <jonathan.berumen@specializedharness.com>
2025-05-20 09:12:17 -0600

    guardar

--- Status_material/My Project/Settings.Designer.vb
+++ Status_material/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Status_material.My.MySettings
+        Friend ReadOnly Property Settings() As Global.Material.My.MySettings
             Get
-                Return Global.Status_material.My.MySettings.Default
+                Return Global.Material.My.MySettings.Default
             End Get
         End Property
     End Module
